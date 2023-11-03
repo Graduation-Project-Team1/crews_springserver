ac1ea1a96c3fd47a4778780029c17f963ea1029a
나종명 <myung0484@naver.com>
2023-11-04 01:07:47 +0900

    20231104 / Page Controller base

---
+++ src/main/java/com/team1/finalproject/common/dto/SearchResponse.java
@@ -0,0 +1,12 @@
+package com.team1.finalproject.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SearchResponse {
+    private String response;
+}
---
+++ src/main/java/com/team1/finalproject/common/service/PageService.java
@@ -0,0 +1,54 @@
+package com.team1.finalproject.common.service;
+
+import com.team1.finalproject.common.dto.*;
+import com.team1.finalproject.memberdata.service.MemberService;
+import com.team1.finalproject.sportsdata.dto.TeamInfoRequest;
+import com.team1.finalproject.sportsdata.dto.TeamInfoResponse;
+import com.team1.finalproject.sportsdata.service.SportsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class PageService {
+
+    private final MemberService memberService;
+    private final SportsService sportsService;
+
+    public MainPageResponse getMainPage(MainPageRequest dto){
+        TeamInfoResponse teamInfo = sportsService.getTeamInfo(new TeamInfoRequest(
+                memberService.viewMemberData(dto.getMemberId()).getTeamId()));
+
+        //get podcast, teamId = teamInfo.getTeamId;
+
+        //get news, num = 3
+
+        //get trend, num = 5
+
+        //get pos, neg
+
+        return null;
+    }
+
+    public NewsPageResponse getNewsPage(NewsPageRequest dto) {
+        return null;
+    }
+
+    public PodcastPageResponse getPodcastPage(PodcastPageRequest dto) {
+        return null;
+    }
+
+    public AnalysisPageResponse getAnalysisPage(AnalysisPageRequest dto) {
+        return null;
+    }
+
+    public MyTeamPageResponse getMyTeamPage(MainPageRequest dto) {
+        return null;
+    }
+
+    public SearchResponse searchQuery(String query) {
+        return null;
+    }
+}
